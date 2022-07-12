2de6a7cfdac5abbf8374e61d210a9cd1f47d7083
Ramiro Nasello <rnasello@hotmail.com>
2022-07-11 21:22:24 -0300

    mi primer commit

--- app.js
+++ app.js
@@ -1,81 +1,10 @@
-const stock = [
-    {
-        "id": 1,
-        "nombre": 'Buzo Red Bull',
-        "precio": 10000,
-        "img": "Fotos/buzorb.jpg"
-    },
-    {
-        "id": 2,
-        "nombre": 'Gorra Red Bull',
-        "precio": 3000,
-        "img": "Fotos/gorra.jpg"
 
-    },
-    {
-        "id": 3,
-        "nombre": 'Taza Red Bull',
-        "precio": 2000,
-        "img":"Fotos/tazarb.jpg"
-    },
-    {
-        "id": 4,
-        "nombre": 'Botella Red Bull',
-        "precio": 2500,
-        "img":"Fotos/botellarb.jpg"
-    },
-    {
-        "id": 5,
-        "nombre": 'Campera Ferrari',
-        "precio": 15000,
-        "img":"Fotos/camperaferrari.jpg"
-    },
-    {
-        "id": 6,
-        "nombre": 'Chomba Ferrari',
-        "precio": 8500,
-        "img": "Fotos/chombaferrari.jpg"
-    },
-    {
-        "id": 7,
-        "nombre": 'Gorra Ferrari',
-        "precio": 2800,
-        "img": "Fotos/gorraferrari.jpg"
-    },
-    {
-        "id": 8,
-        "nombre": 'Mochila Ferrari',
-        "precio": 4500,
-        "img": "Fotos/mochilaferrari.jpg"
-    },
-    {
-        "id": 9,
-        "nombre": 'Canguro Mercedes Benz',
-        "precio": 12000,
-        "img": "Fotos/canguromb.jpg"
-    },
-    {
-        "id": 10,
-        "nombre": 'Remera Mercedes Benz',
-        "precio": 6000,
-        "img": "Fotos/remeramb.jpg"
-    },
-    {
-        "id": 11,
-        "nombre": 'Billetera Mercedes Benz',
-        "precio": 2500,
-        "img":"Fotos/billeteramb.jpg"
-    },{
-        "id": 12,
-        "nombre": 'Gorra Mercedes Benz',
-        "precio": 3000,
-        "img": "Fotos/gorramb.jpg"
-    }
-];
 
 let carrito
 
 let total= 0
+
+let stock= []
 
 
 const carritoEnLS=JSON.parse (localStorage.getItem('carrito'))
@@ -122,14 +51,18 @@ butVaciar.addEventListener("click", () => {
 })
 })
 
-  
+fetch('./stock.json')
+    .then((resp) => resp.json())
+    .then((data) => {
+        stock = data
+    
   
 
-stock.forEach((producto) => {
-    const div = document.createElement('div')
-    div.classList.add('flexCards')
+        stock.forEach((producto) => {
+            const div = document.createElement('div')
+            div.classList.add('flexCards')
 
-    div.innerHTML = `
+            div.innerHTML = `
                     <div id="card${producto.id}">
                     <img src=${producto.img} alt="">
                     <p>${producto.nombre}</p>
@@ -138,8 +71,10 @@ stock.forEach((producto) => {
                     </div>
                 `
 
-    containerProducts.append(div)
-})
+            containerProducts.append(div)
+    })
+
+})    
 
 const agregarAlCarrito = (id) => {
     const item = stock.find( (producto) => producto.id === id)
